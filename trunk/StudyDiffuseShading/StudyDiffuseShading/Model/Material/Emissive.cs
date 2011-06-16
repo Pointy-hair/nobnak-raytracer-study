c7d47b47369a8415260087b11f411ae7caabbe2f
nobu.nakata@gmail.com <nobu.nakata@gmail.com@91256f8b-05aa-54ab-3c8a-fd20fdb4bd79>
2011-06-16 06:07:51 +0000

    add Emissive Material

---
+++ trunk/StudyDiffuseShading/StudyDiffuseShading/Model/Material/Emissive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using StudyDiffuseShading.Model.Sampler;
+using StudyDiffuseShading.Model.Primitive;
+
+namespace StudyDiffuseShading.Model.Material {
+    public class Emissive : IMaterial {
+        private Vector3D ce;
+        private double ls;
+
+
+        public Emissive(Vector3D ce, double ls) {
+            this.ce = ce;
+            this.ls = ls;
+        }
+
+
+        public Vector3D shadeOnPath(Tracer tracer, ISampler sampler, Collision collision) {
+            return ce * ls;
+        }
+    }
+}
---
+++ trunk/StudyDiffuseShading/StudyDiffuseShading/Model/Material/IMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using StudyDiffuseShading.Model.Sampler;
+using StudyDiffuseShading.Model.Primitive;
+
+namespace StudyDiffuseShading.Model.Material {
+    public interface IMaterial {
+        Vector3D shadeOnPath(Tracer tracer, ISampler sampler, Collision collision);
+    }
+}
